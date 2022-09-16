d4e646b22aeeecca4d36d1b6e2d92985fdc22903
Esat <80488498+EsatOZMEN@users.noreply.github.com>
2022-09-16 16:16:09 +0300

    First Commit

---
+++ helloWorld/src/helloWorld/java.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class java {
+
+	public static void main(String[] args) {
+		//intellesence 
+		//kod satırnın bittiğini ; ile belirtmen gerekiyor !!
+		//
+		
+		System.out.println("Hello JAVA");
+		System.out.println("Hello JAVA2");
+
+	}
+
+}
