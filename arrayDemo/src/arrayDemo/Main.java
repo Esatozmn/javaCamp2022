535f3395b35af454524d13c43d9888498f7f26fa
Esat <80488498+EsatOZMEN@users.noreply.github.com>
2022-09-17 00:39:00 +0300

    arrayDemo

---
+++ arrayDemo/src/arrayDemo/Main.java
@@ -0,0 +1,17 @@
+package arrayDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		String[] ogrenciler = new String[3];
+		ogrenciler[0] = "Engin";
+		ogrenciler[1] = "Esat";
+		ogrenciler[2] = "İlayda";
+		for (int i = 0; i < ogrenciler.length; i++) {
+			System.out.println(ogrenciler[i]);
+		}
+
+	}
+
+}
