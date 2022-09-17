d74e36e908be4a8d9b3deb8689a03af7d16e8565
Esat <80488498+EsatOZMEN@users.noreply.github.com>
2022-09-17 21:55:03 +0300

    commit1

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,22 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayı = 2 ;
+		if(sayı<20) {
+		System.out.println("sayı 20 den küçük !!");
+		}
+		else if (sayı ==20){
+			System.out.println("sayı 20 ile eşittir");
+			
+		}else {
+			System.out.println("sayı 20 den büyüktür");
+			
+			
+		}
+		
+		
+	}
+
+}
