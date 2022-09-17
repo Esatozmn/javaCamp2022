c9ad9c8fb1d3be185dda31e1384c654e72082015
Esat <80488498+EsatOZMEN@users.noreply.github.com>
2022-09-17 21:42:21 +0300

    commit1

---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,32 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//boolean true veya false olarak kullanılabilri
+		//char single character kullanılabilir
+		//byte intager / 1 byte / -128 to 127
+		//short intager / 2 bytes / -32768 to  32767
+		//int                    ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//long                   ]sadece kullandıkları veri boyutları farklı olduğu için bellek kullanımından 
+		//float  /ondalık sayı   ]tasarruf için bu farklı kullanımlar mevcuttur 
+		//double /vondalık sayı  ]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		
+		int sayi = 12; 
+		
+		sayi= 2222;                           //alabileceği değeri geçtiği için hata veriyor !!
+		sayi= 222222222222222222222222222222;
+		double  sayi2 = 12.5;
+		sayi = 123 ;
+		char karakter = 'a' ;
+		boolean doğruMu= false ;
+		
+		if (doğruMu= true) {
+			
+		}
+		
+	}
+
+}
